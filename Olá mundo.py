8d892e8fda2097d8cd10eaa22c2f14e5c4003cf5
pedrogarcia1910 <62154822+pedrogarcia1910@users.noreply.github.com>
2020-06-11 17:15:03 -0300

    Delete Olá mundo.py

--- Olá mundo.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask # Importa a biblioteca
-app = Flask(__name__) # Inicializa a aplicação
-
-@app.route("/") # Cria uma rota
-def hello():
-    return "Olá, mundo!"
-
-if __name__ == "__main__":
-    app.run()  # Executa a aplicação
