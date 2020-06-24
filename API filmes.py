641a7cc49e41d57e1085a8c5ea4f7a0daa165ed5
pedrogarcia1910 <62154822+pedrogarcia1910@users.noreply.github.com>
2020-06-24 17:09:15 -0300

    API filmes
    Acesso à página web por meio de reconhecimento de voz.

---
+++ API filmes.py
@@ -0,0 +1,40 @@
+import speech_recognition as sr #importa biblioteca speech_recognition
+import webbrowser
+
+
+
+recognizer = sr.Recognizer()    #inicializa reconhecimento de voz
+microphone = sr.Microphone()    #inicializa microphone
+
+
+
+#reconhecimento de Voz
+def reconhecimento():
+    with microphone as source: 
+    	recognizer.adjust_for_ambient_noise(source)     #calibra o microfone
+    	audio = recognizer.listen(source)				#escuta o microfone				
+    
+    try:
+        retornoAudio = recognizer.recognize_google(audio,language='pt-BR') #interpreta o audio utilizando o idioma português brasileiro
+    except:
+        print("repita novamente...")
+        retornoAudio = reconhecimento()
+            
+    with open("outputs.txt","w") as arquivo:   #abre arquivo 'outuputs.txt' para modo escrita                                  
+    	arquivo.write(retornoAudio) # escreve no arquivo  o retorno do audio
+    
+    return retornoAudio
+
+
+
+def web():
+      
+    ouvindo = reconhecimento().lower()
+    print(ouvindo)
+    if ouvindo == 'página':
+        webbrowser.open('https://topicos-avancados.herokuapp.com/getFilme/4')
+    else:
+        print("ERRO")
+    
+    return web
+    
